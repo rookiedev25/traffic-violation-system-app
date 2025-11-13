f58ea6ee88dcac123188703b781375d8588c31bc
rookiedev25 <rookiedeveloper.at.localhost@gmail.com>
2025-11-13 15:46:43 +0530

    updated

--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
-// Distance between cameras in meters
+// Distance between cameras in meters.
 export const CAMERA_DISTANCE_METERS = 1000;
 
 // Fine mapping as per problem statement
 export const FINE_MAPPING = {
   "2wheeler": 200,
   "4wheeler": 400,
-  "bus": 600,
-  "truck": 800,
+  bus: 600,
+  truck: 800,
 };
 
 // Location speed limits (km/h)
